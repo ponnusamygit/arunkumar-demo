8a82e03593f061f8975c7a7c007701e414324be2
Ponnusamy <ponnusamy.gac@gmail.com>
2024-04-25 02:19:49 +0530

    Student model

---
+++ db/migrate/20240424204942_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :age
+      t.string :class_room
+
+      t.timestamps
+    end
+  end
+end
