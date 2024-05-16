2f82658a4c06aa526d02683624ae4e293556dbab
Ponnusamy K <ponnusamy.gac@gmail.com>
2024-05-16 23:31:36 +0530

    Feature computer

---
+++ db/migrate/20240516180124_create_computers.rb
@@ -0,0 +1,10 @@
+class CreateComputers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :computers do |t|
+      t.string :name
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
